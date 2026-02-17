e60715538934057be0229e8f171349d8f66242ea
chappertron <52535659+chappertron@users.noreply.github.com>
2026-02-17 15:59:41 +0000

    basic boundary command test with snapshot files

---
+++ src/command_syntax/syntaxes/boundary.rs
@@ -0,0 +1,31 @@
+use crate::command_syntax::CommandSyntax;
+
+pub(crate) fn syntax() -> CommandSyntax {
+    // TODO: support requiring specific value for arguments
+
+    let mut syntax = CommandSyntax::new("boundary");
+
+    syntax
+        .add_positional("x")
+        .add_positional("y")
+        .add_positional("z");
+
+    syntax
+}
+
+#[cfg(test)]
+mod test {
+    use itertools::Itertools;
+
+    #[test]
+    fn lmp_examples() {
+        let x = "boundary p p f".split_whitespace().collect_vec();
+        insta::assert_debug_snapshot!(super::syntax().parse(x));
+
+        let x = "boundary p fs p".split_whitespace().collect_vec();
+        insta::assert_debug_snapshot!(super::syntax().parse(x));
+
+        let x = "boundary s f fm".split_whitespace().collect_vec();
+        insta::assert_debug_snapshot!(super::syntax().parse(x));
+    }
+}
--- src/command_syntax/syntaxes/mod.rs
+++ src/command_syntax/syntaxes/mod.rs
@@ -1,2 +1,2 @@
+mod boundary;
 mod create_atoms;
-
